2a82e9ae9e963a3fab64043f48bce71e1aebf707
Taeyang Baek <84064951+j-baek@users.noreply.github.com>
2023-11-20 13:47:10 +0000

    rotation on x, y axis done

--- src/cube.py
+++ src/cube.py
@@ -3,37 +3,49 @@ import time
 import math
 import numpy as np
 
-X_MAX = 261
-X_MIN = 0
-Y_MAX = 0 # first row of terminal is at y = 0
-Y_MIN = 60 # last row of terminal is at y = Y_MIN
+# x from 0 to 260, and y from 0 to 60,
+# but setting the mid point on x,y plane to be origin (x = 0, y = 0)
+X_MAX = 120
+X_MIN = -120
+Y_MAX = -30 # first row of terminal is at y = 0
+Y_MIN = 30 # last row of terminal is at y = Y_MIN
 
-X_MID = 120 # mid of x
-Y_MID = 30  # mid of y
+X_OFFSET = 120
+Y_OFFSET = 30
+
+X_MID = 60
+Y_MID = 15
+
 
 DELAY = 0 # delay of drawing 
 
 class dot: # x,y coordinate
     def __init__(self, x: int, y: int, z: int): 
         # make sure to keep the coordinate within the max and min
+
         if x > X_MAX:
-            x = X_MAX
+            x_m = x % X_MAX # e.g. x = 270, X_MAX = 250, then x_m = 20
+            x = X_MAX - x_m # x = 250 - 20 = 230
         elif x < X_MIN:
-            x = X_MIN
-        if y < Y_MAX: # first row is at y = 0
-            y = Y_MAX
-        elif y > Y_MIN: # last row is at y = Y_MIN
-            y = Y_MIN
+            x = abs(x) % X_MAX # e.g. x = - 270, X_MAX = 250, then x_m = 20
+        
+        if y < Y_MAX:
+            y = abs(y) % Y_MIN # e.g. y = -70, Y_MIN = 60, then y = 10
+        elif y > Y_MIN:
+            y_m = y % Y_MIN # e.g. y = 70, Y_MIN = 60, then y_m = 10
+            y = Y_MIN - y_m # y = 60 - 10 = 50
+
     
         # make sure to get x,y,z to be integer 
         self.x = int(x)
         self.y = int(y)
         self.z = int(z)
+        self.vec = [self.x, self.y, self.z] # vector form of a dot (point)
     
     def draw_dot(self):
         # ANSI escape code used. '\033' is the escape character in Python strings, 
         # which is equivalent to the ASCII value of the escape character.
-        print(f"\033[{self.y};{self.x}H", end="") # move the point to the appropriate position
+        print(f"\033[{self.y + Y_OFFSET};{self.x + X_OFFSET}H", end="") # move the point to the appropriate position
         # '\033[32m' makes the text colour to be green, and '\033[0m' resets the text formmating back to the default
         print("\033[32m*\033[0m") # drawing a star with text color being green
 
@@ -85,80 +97,56 @@ def clear_terminal():
     else: 
         print("operating system: " + os.name)
 
-
-def trd_twod(coord: dot): # get a dot and change it into 2d- vector
-    vec = [coord.x, coord.y, coord.z]
-    return vec
-
-
-def rotate_x(vec: list, angle): # rotate on x-axis
-    #rot_mat_x = [
-    #    [math.cos(angle_rad), -math.sin(angle_rad), 0],
-    #    [math.sin(angle_rad), math.cos(angle_rad), 0]
-    #]
+def rotate_x(vec: list, theta): # rotate on x-axis
 
     rot_mat_x = [
         [1, 0, 0],
-        [0, math.cos(angle), -math.sin(angle)],
-        [0, math.sin(angle), math.cos(angle)]]
+        [0, math.cos(theta), -math.sin(theta)],
+        [0, math.sin(theta), math.cos(theta)]]
 
     result = np.matmul(vec, rot_mat_x)
 
-    return dot(result[0], result[1], 1)
+    return dot(result[0], result[1], result[2])
+
+def rotate_y(vec: list, theta): # rotate on y-axis
+    rot_mat_y = [
+        [math.cos(theta), 0, math.sin(theta)],
+        [0, 1, 0],
+        [-math.sin(theta), 0, math.cos(theta)]
+    ]
+
+    result = np.matmul(vec, rot_mat_y)
+    return dot(result[0], result[1], result[2])
 
 if __name__ == "__main__":
     clear_terminal()
     # 8 vertices of a cube
-    p1 = dot(100,20, 1)
-    p2 = dot(140,20, 1)
-    p3 = dot(100,40, 1)
-    p4 = dot(140,40, 1)
+    p1 = dot(X_MID/3, Y_MID/3,1)
+    p2 = dot(X_MID*2/3, Y_MID/3,1)
+    p3 = dot(X_MID/3, Y_MID*2/3,1)
+    p4 = dot(X_MID*2/3, Y_MID*2/3,1)
 
-    p5 = dot(130,10, 1)
-    p6 = dot(170,10, 1)
-    p7 = dot(130,30, 1)
-    p8 = dot(170,30, 1)
+    for i in range (0, 100):
+        for r in range(0, 360):
+            clear_terminal()
+            angle1 = math.radians(r)
+            angle2 = math.radians(180 + r) # second angle that rotates backward, compared to angle1
+            p1_rot = rotate_x(p1.vec,angle1)
+            p2_rot = rotate_x(p2.vec,angle1)
+            p3_rot = rotate_x(p3.vec,angle2)
+            p4_rot = rotate_x(p4.vec,angle2)
 
-    for r in range(0, 90):
-        clear_terminal()
-        angle = math.radians(r)
-        p1_rot = rotate_x(trd_twod(p1),angle)
-        p2_rot = rotate_x(trd_twod(p2),angle)
-        p3_rot = rotate_x(trd_twod(p3),angle)
-        p4_rot = rotate_x(trd_twod(p4),angle)
-        #p1_rot.draw_dot()
-        #p2_rot.draw_dot()
+            #p1_rot = rotate_y(p1.vec,angle1)
+            #p2_rot = rotate_y(p2.vec,angle2)
+            #p3_rot = rotate_y(p3.vec,angle1)
+            #p4_rot = rotate_y(p4.vec,angle2)
 
-        p1_rot.draw_line(p2_rot)
-        p3_rot.draw_line(p4_rot)
-        
-        p2_rot.draw_line(p4_rot)
-        p3_rot.draw_line(p1_rot)
-        time.sleep(0.01)
-        
-        #p2_rot.draw_line(p4_rot)
-        #p3_rot.draw_line(p1_rot)
-        #p4_rot.draw_line(p2_rot)
-
-
-    #p1.draw_dot()
-    #p2.draw_dot()
-    #p3.draw_dot()
-    #p4.draw_dot()
-
-
-
-
-
-    #p5.draw_dot()
-    #p6.draw_dot()
-    #p7.draw_dot()
-    #p8.draw_dot()
-
-    #p1.draw_line(p2)
-    #p1.draw_line(p3)
-    #p2.draw_line(p4)
-    #p4.draw_line(p3)
+            p1_rot.draw_line(p2_rot)
+            p3_rot.draw_line(p4_rot)
+            
+            p2_rot.draw_line(p4_rot)
+            p3_rot.draw_line(p1_rot)
+            time.sleep(0.01)
 
 
 
