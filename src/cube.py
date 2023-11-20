f4eedd4b0d61dedcbcdcb6893eea2699c5bd8997
Taeyang Baek <84064951+j-baek@users.noreply.github.com>
2023-11-20 11:25:11 +0000

    work in progress, trying to rotate on x-axis

--- src/cube.py
+++ src/cube.py
@@ -1,6 +1,7 @@
 import os
 import time
 import math
+import numpy as np
 
 X_MAX = 261
 X_MIN = 0
@@ -10,10 +11,10 @@ Y_MIN = 60 # last row of terminal is at y = Y_MIN
 X_MID = 120 # mid of x
 Y_MID = 30  # mid of y
 
-DELAY = 0.01 # delay of drawing 
+DELAY = 0 # delay of drawing 
 
 class dot: # x,y coordinate
-    def __init__(self, x, y): 
+    def __init__(self, x: int, y: int, z: int): 
         # make sure to keep the coordinate within the max and min
         if x > X_MAX:
             x = X_MAX
@@ -24,8 +25,10 @@ class dot: # x,y coordinate
         elif y > Y_MIN: # last row is at y = Y_MIN
             y = Y_MIN
     
-        self.x = x
-        self.y = y
+        # make sure to get x,y,z to be integer 
+        self.x = int(x)
+        self.y = int(y)
+        self.z = int(z)
     
     def draw_dot(self):
         # ANSI escape code used. '\033' is the escape character in Python strings, 
@@ -60,16 +63,17 @@ class dot: # x,y coordinate
                     y = round(slope*(x - x_i)) + y_i # y = k(x - x_i) + y_i
                 else: # when slope is 0 
                     y = y_i
-                print("x = " + str(x) + " y = " + str(y))
-                coord = dot(x,y)
+                print("x = " + str(x) + " y = " + str(y) + " z = " + str(self.z))
+                coord = dot(x,y, self.z)
                 #print("x = " + str(coord.x) + " y = " + str(coord.y))
                 coord.draw_dot()
                 time.sleep(DELAY)
         else: # draw a vertical line
             for y in range(self.y, end_c.y, c_y):
-                coord = dot(end_c.x, y)
+                coord = dot(end_c.x, y, self.z)
                 coord.draw_dot()
                 time.sleep(DELAY)
+
 
 
 # clear terminal before display an ojbect on terminal
@@ -81,44 +85,82 @@ def clear_terminal():
     else: 
         print("operating system: " + os.name)
 
+
+def trd_twod(coord: dot): # get a dot and change it into 2d- vector
+    vec = [coord.x, coord.y, coord.z]
+    return vec
+
+
+def rotate_x(vec: list, angle): # rotate on x-axis
+    #rot_mat_x = [
+    #    [math.cos(angle_rad), -math.sin(angle_rad), 0],
+    #    [math.sin(angle_rad), math.cos(angle_rad), 0]
+    #]
+
+    rot_mat_x = [
+        [1, 0, 0],
+        [0, math.cos(angle), -math.sin(angle)],
+        [0, math.sin(angle), math.cos(angle)]]
+
+    result = np.matmul(vec, rot_mat_x)
+
+    return dot(result[0], result[1], 1)
+
 if __name__ == "__main__":
     clear_terminal()
     # 8 vertices of a cube
-    p1 = dot(100,20)
-    p2 = dot(140,20)
-    p3 = dot(100,40)
-    p4 = dot(140,40)
+    p1 = dot(100,20, 1)
+    p2 = dot(140,20, 1)
+    p3 = dot(100,40, 1)
+    p4 = dot(140,40, 1)
 
-    p5 = dot(130,10)
-    p6 = dot(170,10)
-    p7 = dot(130,30)
-    p8 = dot(170,30)
+    p5 = dot(130,10, 1)
+    p6 = dot(170,10, 1)
+    p7 = dot(130,30, 1)
+    p8 = dot(170,30, 1)
 
-    p1.draw_dot()
-    p5.draw_dot()
-    p1.draw_dot()
-    p2.draw_dot()
-    p3.draw_dot()
-    p4.draw_dot()
-    p5.draw_dot()
-    p6.draw_dot()
-    p7.draw_dot()
-    p8.draw_dot()
+    for r in range(0, 90):
+        clear_terminal()
+        angle = math.radians(r)
+        p1_rot = rotate_x(trd_twod(p1),angle)
+        p2_rot = rotate_x(trd_twod(p2),angle)
+        p3_rot = rotate_x(trd_twod(p3),angle)
+        p4_rot = rotate_x(trd_twod(p4),angle)
+        #p1_rot.draw_dot()
+        #p2_rot.draw_dot()
 
-    p1.draw_line(p2)
-    p1.draw_line(p3)
-    p2.draw_line(p4)
-    p4.draw_line(p3)
+        p1_rot.draw_line(p2_rot)
+        p3_rot.draw_line(p4_rot)
+        
+        p2_rot.draw_line(p4_rot)
+        p3_rot.draw_line(p1_rot)
+        time.sleep(0.01)
+        
+        #p2_rot.draw_line(p4_rot)
+        #p3_rot.draw_line(p1_rot)
+        #p4_rot.draw_line(p2_rot)
 
-    p5.draw_line(p6)
-    p6.draw_line(p8)
-    p8.draw_line(p7)
-    p7.draw_line(p5)
 
-    p1.draw_line(p5)
-    p2.draw_line(p6)
-    p3.draw_line(p7)
-    p4.draw_line(p8)
+    #p1.draw_dot()
+    #p2.draw_dot()
+    #p3.draw_dot()
+    #p4.draw_dot()
+
+
+
+
+
+    #p5.draw_dot()
+    #p6.draw_dot()
+    #p7.draw_dot()
+    #p8.draw_dot()
+
+    #p1.draw_line(p2)
+    #p1.draw_line(p3)
+    #p2.draw_line(p4)
+    #p4.draw_line(p3)
+
+
 
 
 else:
