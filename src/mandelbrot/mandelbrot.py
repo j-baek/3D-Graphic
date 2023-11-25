86b3cc5ed04c64d1ed461c9e1b2bf96aae48381b
Taeyang Baek <84064951+j-baek@users.noreply.github.com>
2023-11-25 14:49:01 +0000

    in progress..

--- src/mandelbrot/mandelbrot.py
+++ src/mandelbrot/mandelbrot.py
@@ -1,6 +1,6 @@
 import os 
-import math
 import time
+import numpy as np
 
 X_MAX = 260 # x = column
 Y_MAX = 60 # y = row
@@ -29,50 +29,50 @@ def clear_terminal():
     else: 
         print("operating system: " + os.name)
 
-def draw_point(x,y):
-    x = int(x)
-    y = int(y)
-    # moving cursor to appropriate position
-    # Y_MIN - y value is for setting y position upside down as in terminal, y increases as it goes down
-    print(f"\033[{Y_MIN - y+Y_OFF_SET};{x+X_OFF_SET}H", end="") 
-    print("\033[32m" + "." + "\033[0m") # draw dot 
 
-def x_square_func(x):
-    y = x*x*SLOPE
-    return y
+def is_point_in_m_set(x,y, max_iteration):
+    # complex number takes (real, imag), so if x = 3, y =2, z0 = 3 + 2j, where j indicates imaginary number
+    z0 = complex(x,y) 
+    z = 0 # initial val of z
 
-def draw_function(math_func, scale): # take math funciton and scale for zooming
-    for x in range(X_NEG_MAX, X_POS_MAX):
-        if x < X_NEG_MAX: 
-            x = X_NEG_MAX
-        elif x > X_POS_MAX:
-            x = X_POS_MAX
-        y = math_func(x) * scale
-        if y >= Y_NEG_MAX and y <= Y_POS_MAX:
-            draw_point(x,y)
-
-def zoom_in():
-    for i in range(1, ZOOM_RANGE):
-        scale = 1/i
-        draw_function(x_square_func, scale)
-        time.sleep(TIME_DELAY)
-        clear_terminal()
-
-def zoom_out():
-    for i in range(ZOOM_RANGE, 0, -1):
-        scale = 1/i
-        draw_function(x_square_func, scale)
-        time.sleep(TIME_DELAY)
-        clear_terminal()
-
+    for i in range(0, max_iteration):
+        z = z**2 + z0
+        if abs(z) > 2:
+            return False
+    return True
 
 if __name__ == "__main__":
     clear_terminal()
+    matrix = [["."]*X_POS_MAX for _ in range(Y_MAX)] # make a matrix with row = Y_MAX and col = X_MAX
+    max_iteration = 100
+    x = -2
+    for row in range(len(matrix)):
+        y = -2
+        for col in range(len(matrix[0])):
+            z = complex(x,y)
+            in_m_set = is_point_in_m_set(x,y, max_iteration)
+            if in_m_set == True:
+                matrix[row][col] = "@"
+            else:
+                matrix[row][col] = " "
+            y += 0.066666666666* col
+        x += 0.03076923076 * row
+    
+    for row in range(len(matrix)):
+        for col in range(len(matrix[0])):
+            print(matrix[row][col], end=" ")
 
-    draw_function(x_square_func, 1)
 
-    zoom_in()
-    clear_terminal()
-    print("now zooming out")
-    time.sleep(3)
-    zoom_out()
+
+
+
+
+
+    '''
+    max_iteration = 1000
+    for x in range(X_NEG_MAX, X_POS_MAX):
+        for y in range(Y_NEG_MAX, Y_POS_MAX):
+            in_m_set = is_point_in_m_set(x,y, max_iteration)
+            if in_m_set == True:
+                draw_point(x,y)
+    '''
