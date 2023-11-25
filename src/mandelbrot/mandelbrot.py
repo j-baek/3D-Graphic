f0ad1bcc0decdb9cc27e3eb62c22bf8bb9d64659
Taeyang Baek <84064951+j-baek@users.noreply.github.com>
2023-11-25 13:38:35 +0000

    mandelbrot in progress

--- src/mandelbrot/mandelbrot.py
+++ src/mandelbrot/mandelbrot.py
@@ -1,5 +1,6 @@
 import os 
 import math
+import time
 
 X_MAX = 260 # x = column
 Y_MAX = 60 # y = row
@@ -15,6 +16,9 @@ SLOPE = 1/4 # since y is about 1/4 smaller than x, to get the proportion right, 
 
 X_OFF_SET = 120
 Y_OFF_SET = 30
+
+ZOOM_RANGE = 300
+TIME_DELAY = 0.01
 
 # clear terminal before display an ojbect on terminal
 def clear_terminal():
@@ -37,27 +41,38 @@ def x_square_func(x):
     y = x*x*SLOPE
     return y
 
-def sin_func(x):
-    x = math.radians(x)
-    y = math.sin(x)
-    return y
-
-
-
-def draw_function(math_func):
+def draw_function(math_func, scale): # take math funciton and scale for zooming
     for x in range(X_NEG_MAX, X_POS_MAX):
-        y = math_func(x)
+        if x < X_NEG_MAX: 
+            x = X_NEG_MAX
+        elif x > X_POS_MAX:
+            x = X_POS_MAX
+        y = math_func(x) * scale
         if y >= Y_NEG_MAX and y <= Y_POS_MAX:
             draw_point(x,y)
 
+def zoom_in():
+    for i in range(1, ZOOM_RANGE):
+        scale = 1/i
+        draw_function(x_square_func, scale)
+        time.sleep(TIME_DELAY)
+        clear_terminal()
 
-
-
-
+def zoom_out():
+    for i in range(ZOOM_RANGE, 0, -1):
+        scale = 1/i
+        draw_function(x_square_func, scale)
+        time.sleep(TIME_DELAY)
+        clear_terminal()
 
 
 if __name__ == "__main__":
     clear_terminal()
 
-    draw_function(x_square_func)
-    draw_function(sin_func)
+    draw_function(x_square_func, 1)
+
+    zoom_in()
+    clear_terminal()
+    print("now zooming out")
+    time.sleep(3)
+    zoom_out()
