304d3dcbbe2898add0b0a5e982006e0e841a35f1
Taeyang Baek <84064951+j-baek@users.noreply.github.com>
2023-11-27 13:49:56 +0000

    mandelbrot fractal done

--- src/mandelbrot/mandelbrot.py
+++ src/mandelbrot/mandelbrot.py
@@ -1,24 +1,10 @@
 import os 
-import time
-import numpy as np
 
-X_MAX = 260 # x = column
-Y_MAX = 60 # y = row
-X_MIN = 0
-Y_MIN = 0
+X_MAX = 240 # x = column
+Y_MAX = 56 # y = row
 
 X_POS_MAX = 120
-X_NEG_MAX = -120
-Y_POS_MAX = 30
-Y_NEG_MAX = -30
-
-SLOPE = 1 # since y is about 1/4 smaller than x, to get the proportion right, multiply slope when drawing a graph
-
-X_OFF_SET = 120
-Y_OFF_SET = 30
-
-ZOOM_RANGE = 300
-TIME_DELAY = 0.01
+Y_POS_MAX = 28
 
 # clear terminal before display an ojbect on terminal
 def clear_terminal():
@@ -50,25 +36,51 @@ def get_ascii(iteration, max_iteration): # get ascii character based on the iter
     
     return ascii_char[i]
 
+def zoom(matrix, type, zoom_iter_max, max_iteration): # zoom in or out, or stay not zoomed, depending on type
+    # type 0: no zoom
+    # type >= 1: zoom in
+    # type =< -1: zoom out
+    zoom_iter = 0
+    zoom_iter_max = abs(zoom_iter_max) # make sure to get zoom iteration ends
+
+    if type == 0:
+        zoom_iter = zoom_iter_max # render fractal just once
+        scale = 1
+        zoom_scale = 1
+    if type >= 1:
+        scale = 0.5
+        zoom_scale = 0.1
+    if type <= -1:
+        scale = 20
+        zoom_scale = -0.1
+        
+    while(zoom_iter <= zoom_iter_max):
+        for row in range(len(matrix)):
+            for col in range(len(matrix[0])):
+                # col - X_POS_MAX and row - Y_POS_MAX centers the image at the origin (0,0) in mathematical space
+                # scaling factor scales x and y accordingly. e.g. to set x and y to range from (-2,2), set scaling factor to be 2.
+                # e.g. when col = 260 (at max), then x = (260 - 130) / (130/2) = 130 / (130/2) = 130 * 2 / 130 = 2. 
+                x = (col - X_POS_MAX) * (scale/X_POS_MAX)
+                y = (row - Y_POS_MAX) * (scale/Y_POS_MAX)
+
+                m_set_iteration = mandelbrot_iteration(x,y, max_iteration)
+                matrix[row][col] = get_ascii(m_set_iteration, max_iteration)
+
+        clear_terminal() # clear terminal before rendering
+
+        # display mandelbrot fractal
+        for row in range(len(matrix)):
+            for col in range(len(matrix[0])):
+                print(matrix[row][col], end="")
+            print() # for new line for every row
+        
+        zoom_iter += 1
+        scale += zoom_scale
+
 
 if __name__ == "__main__":
     clear_terminal()
     matrix = [["."]*X_MAX for _ in range(Y_MAX)] # make a matrix with row = Y_MAX and col = X_MAX
-    max_iteration = 5000
-
-    for row in range(len(matrix)):
-        for col in range(len(matrix[0])):
-            # col - X_POS_MAX and row - Y_POS_MAX centers the image at the origin (0,0) in mathematical space
-            # scaling factor scales x and y accordingly. e.g. to set x and y to range from (-2,2), set scaling factor to be 2.
-            # e.g. when col = 260 (at max), then x = (260 - 130) / (130/2) = 130 / (130/2) = 130 * 2 / 130 = 2. 
-            scale = 1.5
-            x = (col - X_POS_MAX) * (scale/X_POS_MAX)
-            y = (row - Y_POS_MAX) * (scale/Y_POS_MAX)
-
-            m_set_iteration = mandelbrot_iteration(x,y, max_iteration)
-            matrix[row][col] = get_ascii(m_set_iteration, max_iteration)
-
-    for row in range(len(matrix)):
-        for col in range(len(matrix[0])):
-            print(matrix[row][col], end=" ")
-        print() # for new line for every row
+    max_iteration = 1000
+    
+    zoom(matrix, -1, 200, max_iteration)
