753e667e803a6186f5e8c485b10c4b1094dede47
Taeyang Baek <84064951+j-baek@users.noreply.github.com>
2023-11-27 14:39:54 +0000

    mandelbrot fractal all done

--- src/mandelbrot/mandelbrot.py
+++ src/mandelbrot/mandelbrot.py
@@ -6,6 +6,9 @@ Y_MAX = 56 # y = row
 X_POS_MAX = 120
 Y_POS_MAX = 28
 
+X_CENTER = 0.3001301496 # found this point on youtube
+Y_CENTER = 0.025500022629245 # found this point on youtube
+
 # clear terminal before display an ojbect on terminal
 def clear_terminal():
     if os.name == 'posix': # Linux or macOS
@@ -15,7 +18,7 @@ def clear_terminal():
     else: 
         print("operating system: " + os.name)
 
-def mandelbrot_iteration(x,y, max_iteration): # number of iterations it takes to fall out of mandelbrot set
+def in_mandelbrot_set(x,y, max_iteration): # calculate if the point is in mandelbrot set
     # complex number takes (real, imag), so if x = 3, y =2, z0 = 3 + 2j, where j indicates imaginary number
     z0 = complex(x,y) 
     z = 0 # initial val of z
@@ -23,23 +26,13 @@ def mandelbrot_iteration(x,y, max_iteration): # number of iterations it takes to
     for i in range(0, max_iteration):
         z = z**2 + z0
         if abs(z) > 2:
-            return i
-    return max_iteration 
-
-def get_ascii(iteration, max_iteration): # get ascii character based on the iterations. the darker it is, the more iteration it tkaes
-    ascii_char = [" ", ",", "-", "~", ":", ";", "=", "!", "*","#", "$", "@"] #darkest to brightest
-    if iteration == 0:
-        iteration = 1
-    i = int(max_iteration/iteration) -1 # index starts at 0 so - 1 required
-    if i >= len(ascii_char):
-        i = len(ascii_char) -1
-    
-    return ascii_char[i]
+            return False
+    return True 
 
 def zoom(matrix, type, zoom_iter_max, max_iteration): # zoom in or out, or stay not zoomed, depending on type
     # type 0: no zoom
-    # type >= 1: zoom in
-    # type =< -1: zoom out
+    # type >= 1: zoom out
+    # type =< -1: zoom in
     zoom_iter = 0
     zoom_iter_max = abs(zoom_iter_max) # make sure to get zoom iteration ends
 
@@ -48,23 +41,27 @@ def zoom(matrix, type, zoom_iter_max, max_iteration): # zoom in or out, or stay 
         scale = 1
         zoom_scale = 1
     if type >= 1:
-        scale = 0.5
-        zoom_scale = 0.1
+        scale = 0.0001
+        zoom_scale = 0.5
     if type <= -1:
-        scale = 20
-        zoom_scale = -0.1
+        scale = 5
+        zoom_scale = 1.5
         
-    while(zoom_iter <= zoom_iter_max):
+    while(zoom_iter < zoom_iter_max):
         for row in range(len(matrix)):
             for col in range(len(matrix[0])):
                 # col - X_POS_MAX and row - Y_POS_MAX centers the image at the origin (0,0) in mathematical space
                 # scaling factor scales x and y accordingly. e.g. to set x and y to range from (-2,2), set scaling factor to be 2.
                 # e.g. when col = 260 (at max), then x = (260 - 130) / (130/2) = 130 / (130/2) = 130 * 2 / 130 = 2. 
-                x = (col - X_POS_MAX) * (scale/X_POS_MAX)
-                y = (row - Y_POS_MAX) * (scale/Y_POS_MAX)
+                # need to center and zoom properly to see the patterns continuously
+                x = (col - X_POS_MAX) * (scale/X_POS_MAX) + X_CENTER
+                y = (row - Y_POS_MAX) * (scale/Y_POS_MAX) - Y_CENTER
 
-                m_set_iteration = mandelbrot_iteration(x,y, max_iteration)
-                matrix[row][col] = get_ascii(m_set_iteration, max_iteration)
+                in_m_set = in_mandelbrot_set(x,y, max_iteration)
+                if in_m_set:
+                    matrix[row][col] = "."
+                else:
+                    matrix[row][col]= " "
 
         clear_terminal() # clear terminal before rendering
 
@@ -75,7 +72,9 @@ def zoom(matrix, type, zoom_iter_max, max_iteration): # zoom in or out, or stay 
             print() # for new line for every row
         
         zoom_iter += 1
-        scale += zoom_scale
+        scale /= zoom_scale
+    
+    print(scale)
 
 
 if __name__ == "__main__":
@@ -83,4 +82,4 @@ if __name__ == "__main__":
     matrix = [["."]*X_MAX for _ in range(Y_MAX)] # make a matrix with row = Y_MAX and col = X_MAX
     max_iteration = 1000
     
-    zoom(matrix, -1, 200, max_iteration)
+    zoom(matrix, -1, 500, max_iteration)
